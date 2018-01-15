0830be5878b46c93029e03ab254ec0122abb93d4
manhnguyenvn <manhnguyenvn@yahoo.com>
2018-01-15 19:29:40 +0700

    adding files

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+ return array (
+  'DB_CONNECTION' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_DATABASE' => 'script',
+  'DB_USERNAME' => 'root',
+  'DB_PASSWORD' => '',
+  'ADMIN_USERNAME' => 'admin',
+  'ADMIN_PASSWORD' => 'admin',
+);
